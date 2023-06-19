6ec53491bec359f24f0a9ebbc8796e11b7f3794a
Shanuka Amarasinghe <99233083+Shanuka-A@users.noreply.github.com>
2023-06-19 12:06:09 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -71,7 +71,7 @@ const Banner = () => {
                 viewport={{ once: false, amount: 0.7 }}
                 className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0"
               >
-                <a href="https://github.com/it21302176">
+                <a href="https://github.com/Shanuka-A">
                   <FaGithub />
                 </a>
                 <a href="http://linkedin.com/in/shanuka-udara-1b886a216">
